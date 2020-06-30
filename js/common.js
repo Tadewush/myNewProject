f0dc6c2448ff0ccee1359a8a52bf2b6644e9eec0
Tadewush <slawagladkih88@gmail.com>
2020-06-30 18:32:20 +0300

    new_section

--- js/common.js
+++ js/common.js
@@ -4,11 +4,16 @@ $(document).ready(function() {
 		$(this).after("<div class='mc_item_wrap_af'></div>");		
 	});
 	$(".mc_toggle").click(function() {
-		$("body .mc_wrap .mc_item_wrap > ul, .mc_item_wrap_af").hide();
-		$(".mc_item_wrap").removeClass("active");
-		$(this).parent().parent().addClass("active");
-		$(this).parent().parent().children("ul").show();
-		$(this).parent().parent().children(".mc_item_wrap_af").show(0);
+		if($(this).parent().parent().children("ul").is(":visible")) {
+			$(this).parent().parent().children("ul").slideUp();
+			$(this).parent().parent().children(".mc_item_wrap_af").hide();
+		} else {	
+			$("body .mc_wrap .mc_item_wrap > ul, .mc_item_wrap_af").hide();
+			$(".mc_item_wrap").removeClass("active");
+			$(this).parent().parent().addClass("active");
+			$(this).parent().parent().children("ul").slideDown();
+			$(this).parent().parent().children(".mc_item_wrap_af").show();
+		}	
 	});
 
 	//Цели для Яндекс.Метрики и Google Analytics
